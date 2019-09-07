974757ac4fd18213f78e2e3f2ca5d76f542dfe0f
Grayfield Moningkey <grayfieldmm@gmail.com>
2019-09-07 21:52:36 +0700

    setup cli commands on index.js

--- index.js
+++ index.js
@@ -1,15 +1,56 @@
-const x = (n, fn) => n.reduce((prev, next) => fn(prev, next), 0);
+const prompts = require('prompts');
 
-const add = (x, y) => {
-  return Number(x) + Number(y);
-};
-
-const sum = (...n) => x(n, add);
-
-const multiply = (x, y) => x * y;
-
-module.exports = {
+const {
+  add,
+  multiply,
   sum,
-  multiply
+  product
+} = require('./math');
+const { firstPrimeNumbers } = require('./primes');
+const { firstFibonacciNumbers } = require('./fibonacci');
+
+function help() {
+  console.log('Commands:');
+  console.log('add <x> <y>');
+  console.log('sum <x1> <x2> ... <xn>');
+  console.log('multiply <x> <y>');
+  console.log('product <x1> <x2> ... <xn>');
+  console.log('primes <n>');
+  console.log('fibonacci <n>');
 }
 
+function run (command, params) {
+  console.log({ command, params });
+  const result = command(...params);
+  console.log(result.length > 1 ? result.join(', ') : result);
+}
+
+(function main() {
+  const args = process.argv;
+  const command = args[2];
+  const params = args.slice(3);
+  switch (command) {
+    case 'add':
+      run(add, params);
+      break;
+    case 'sum':
+      run(sum, params);
+      break;
+    case 'multiply':
+      run(multiply, params)
+      break;
+    case 'product':
+      run(product, params);
+      break;
+    case 'primes':
+      run(firstPrimeNumbers, params);
+      break;
+    case 'fibonacci':
+      run(firstFibonacciNumbers, params);
+      break;
+    default: {
+      help();
+      break;
+    };
+  }
+})();
