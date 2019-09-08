b750260f219d89d1aefe4173c2890a0784a6a366
Grayfield Moningkey <grayfieldmm@gmail.com>
2019-09-08 13:45:37 +0700

    handle no parameter by implement default value

--- math.js
+++ math.js
@@ -1,16 +1,16 @@
-const x = (n, fn) => n.reduce((prev, next) => fn(prev, next));
+const x = (n, fn, initValue) => n.reduce((prev, next) => fn(prev, next), initValue);
 
-const add = (x, y) => {
+const add = (x= 0, y = 0) => {
   return Number(x) + Number(y);
 };
 
-const sum = (...n) => x(n, add);
+const sum = (...n) => x(n, add, 0);
 
-const multiply = (x, y) => {
+const multiply = (x = 1, y = 1) => {
   return Number(x) * Number(y);
 };
 
-const product = (...n) => x(n, multiply);
+const product = (...n) => x(n, multiply, 1);
 
 module.exports = {
   add,
--- math.test.js
+++ math.test.js
@@ -16,7 +16,11 @@ describe('add(x, y)', () => {
   it('should return the addtion of x and y even though the value is string ', () => {
     assert.strictEqual(add('-3', '4'), 1);
     assert.strictEqual(add(392.25, '153.5'), 545.75);
-  })
+  });
+  it('should default with 0 if there is no value of y', () => {
+    assert.strictEqual(add(), 0);
+    assert.strictEqual(add(13), 13);
+  });
 });
 
 describe('sum(...n)', () => {
@@ -28,6 +32,9 @@ describe('sum(...n)', () => {
     const n = [3, '15', 42.25, '32.25', '-12.5'];
     assert.strictEqual(sum(...n), 80);
   });
+  it('should default return 0 when there is no parameter', () => {
+    assert.strictEqual(sum(), 0);
+  });
 });
 
 describe('multiply(x, y)', () => {
@@ -36,11 +43,22 @@ describe('multiply(x, y)', () => {
     assert.strictEqual(multiply(327.94, 982.63), 322243.6822);
     assert.strictEqual(multiply(-1, 32), -32);
   });
+  it('should return the product of x and y even though the value is string ', () => {
+    assert.strictEqual(multiply('-3', '4'), -12);
+    assert.strictEqual(multiply(12.25, '4.5'), 55.125);
+  });
+  it('should default with 1 if there is no value of y', () => {
+    assert.strictEqual(multiply(), 1);
+    assert.strictEqual(multiply(13), 13);
+  });
 });
 
 describe('product(...n)', () => {
   it('should return the product of the values', () => {
     assert.strictEqual(product(2, 16, 32), 1024);
     assert.strictEqual(product(4, 17.25, -15, -1), 1035);
+  });
+  it('should default return 1 when there is no parameter', () => {
+    assert.strictEqual(product(), 1);
   });
 });
